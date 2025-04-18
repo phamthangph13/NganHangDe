845bf872a4647760d9900edc188bbd00d653e8e6
Pham Thang <winnieph13@gmail.com>
2025-04-18 18:07:00 +0700

    update

---
+++ src/app/exam/exam-result/exam-result.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exam-result',
+  imports: [],
+  templateUrl: './exam-result.component.html',
+  styleUrl: './exam-result.component.css'
+})
+export class ExamResultComponent {
+
+}
